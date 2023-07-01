edecfa6c719087fa2ba832c60bd307b37691d430
Matias Esteva <estevamatias@hotmail.com>
2023-06-30 21:21:48 -0300

    Corrección del archivo generar.sh

--- generar.sh
+++ generar.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-function generarImagenes {	
-	curl -L https://raw.githubusercontent.com/fernandezpablo85/name_suggestions/master/assets/dict.csv --output ArchivoNombres
-	LINEAS=$(wc -l ArchivoNombres | cut -d' ' -f 1)
-	
+generarImagenes () {
 
-	if [[ -e images ]];then echo "La carpeta ya existe" && exit 1; else mkdir images; fi
+	curl -L https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv --output "ArchivoNombres"
+
+	LINEAS=$(wc -l ArchivoNombres | cut -d' ' -f 1)
+
+	if [ -e images ]
+	then
+		rm -r images
+		mkdir -p images
+	else
+		mkdir -p images
+	fi
+
+
 	for i in $(seq 1 $1)
-	do
+	do	
+		LIMITE=$(($RANDOM % $LINEAS))
+		NOMBRE_RANDOM=$(sed -n "$LIMITE"p ArchivoNombres | cut -d',' -f 1 | tr -d ' ')
 		cd images
-		curl -L https://source.unsplash.com/random/900%C3%97700/?person --output $RANDOM 
-	sleep 1
+		curl -L https://source.unsplash.com/random/900%C3%97700/?person --output "$NOMBRE_RANDOM"
+		sleep 1
+		cd ..
 	done
 }
 
