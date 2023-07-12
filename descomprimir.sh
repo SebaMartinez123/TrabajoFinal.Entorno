4ad270fea6cf547eb30a31b9efe533eb1647b34b
Matias Esteva <estevamatias@hotmail.com>
2023-07-12 19:45:35 -0300

    Se completó la función descomprimir.sh

--- descomprimir.sh
+++ descomprimir.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 descomprimir () {
+	if ! [ $# -eq 2 ];then echo "Se deben ingresar dos argumentos" && return 2; fi
+	if ! [ -d $1 ]; then echo "$1 no es un directorio" && return 2; fi
+	if ! [ -f $1/$2 ]; then echo "$2 no es un archivo" && return 2; fi
+
 	cd `pwd`/$1
 
-	for archivo in *.gz;do md5sum -c $archivo $2 && gunzip $archivo; done
+	md5sum -c $2
+	if [[ $? -eq 0 ]]
+	then
+      		for archivo in *.gz
+		do	
+			gunzip $archivo
+		done
+	else
+       		echo "La suma de verificación de uno de los archivos no coincide" && return 1
+	fi
 }
 
 descomprimir $1 $2
